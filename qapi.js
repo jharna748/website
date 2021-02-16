17bda559d4a62524f3c127977de91e3a3cb25d45
jharna748 <77603642+jharna748@users.noreply.github.com>
2021-02-16 15:04:03 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"602771fdbfd603001f71d84a",
+				"602771fdbfd603001f71d84c"
+			]
+		},
+		"602771fdbfd603001f71d84a": {
+			"id": "602771fdbfd603001f71d84a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"602771fdbfd603001f71d84c": {
+			"id": "602771fdbfd603001f71d84c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,95 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Image, Box, Text } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { Override, Menu, Section, StackItem, Stack } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+			>
+				<Image width="256px" height="256px" src="https://uploads.quarkly.io/602771fdbfd603001f71d847/images/ASHVATTHA%20Logo%20not%20curved(use%20only%20for%20editing%20text).jpg?v=2021-02-16T06:18:08.698Z" />
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+				>
+					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px" />
+					<Override slot="link-index">
+						home
+					</Override>
+					<Override slot="link-404">
+						rdnc mittal foundation
+					</Override>
+				</Menu>
+			</Box>
+		</Section>
+		<Section
+			background="--color-light"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--green" letter-spacing="1px" text-transform="uppercase" margin="0">
+						nurturing compassionate and courageous educators
+					</Text>
+					<Text
+						as="h1"
+						font="--headline1"
+						md-font="--headline2"
+						margin="10px 0"
+						color="#127fec"
+					>
+						Creating opportunities for pre-service teachers
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
